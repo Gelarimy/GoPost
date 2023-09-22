d2b19f02df4c1a42c8d65251d7bfa4c606a8a737
Catherine <gelarimy@gmail.com>
2023-09-22 16:50:13 +0300

    adding ui

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'test.ui'
+# Form implementation generated from reading uiMainWin file 'test.uiMainWin'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
